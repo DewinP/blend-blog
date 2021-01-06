25c39c6c06191d3d09f52f3cbf97644c3ab77a5a
DewinP <dewinspena@gmail.com>
2021-01-06 16:50:27 -0500

    setting up routes

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 // import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 import ormconfig from "./ORMconfig";
 const PORT = 4000;
 
 const main = async () => {
-  const connection = await createConnection(ormconfig);
+  await createConnection(ormconfig);
   const app = express();
-
-  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(bodyParser.json());
+  app.get("/", (_, res) => {
+    res.json("TAP TAP TAP IN");
+  });
 
   app.listen(PORT, () => {
     console.log(`server started in https://localhost:${PORT}`);
