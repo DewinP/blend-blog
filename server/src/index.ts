89efc4ea86b7fb31b298ca19003f1ed92208a94c
DewinP <dewinspena@gmail.com>
2021-01-11 09:07:43 -0500

    Updated config fles

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,10 @@ import cors from "cors";
 import morgan from "morgan";
 import session from "express-session";
 import bodyParser from "body-parser";
-import { ORMConfig } from "./config";
+import { ORMConfig, PORT } from "./config";
 import authRoutes from "./routes/auth.routes";
 import postRoutes from "./routes/post.routes";
 import userRoutes from "./routes/user.routes";
-import passport from "passport";
 
 const main = async () => {
   await createConnection(ORMConfig);
@@ -29,19 +28,14 @@ const main = async () => {
         resave: false,
         saveUninitialized: true,
       })
-    )
-    .use(passport.initialize())
-    .use(passport.session());
+    );
 
-  app.get("/", (_, res) => {
-    res.json("INDEX PAGE");
-  });
   app.use("/api/auth", authRoutes);
   app.use("/api/posts", postRoutes);
   app.use("/api/users", userRoutes);
 
-  app.listen(8080, () => {
-    console.log(`server started in https://localhost:8080`);
+  app.listen(PORT, () => {
+    console.log(`server started in https://localhost:${PORT}`);
   });
 };
 
